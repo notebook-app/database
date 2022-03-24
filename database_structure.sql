40177500ef9c1adebcffdaff4063f3828c42daf3
hubert <kuchhubert@gmail.com>
2022-03-24 20:38:44 +0100

    Change dialect to postgres

--- database_structure.sql
+++ database_structure.sql
@@ -1,26 +1,26 @@
-create database if not exists notebook;
+create database notebook;
 
 create table if not exists users
 (
-    id       int  not null auto_increment primary key,
+    id       bigserial primary key,
     username text not null,
     password text not null
 );
 
 create table if not exists groups(
-    id                              int not null auto_increment primary key,
+    id                              bigserial primary key,
     title                           text default 'unnamed',
-    user_id                         int not null,
-    parent_group_id                 int not null,
+    user_id                         bigint not null,
+    parent_group_id                 bigint not null,
     foreign key (user_id)           references users(id),
     foreign key (parent_group_id)   references groups(id)
 );
 
 
 create table notes (
-    id                      int not null auto_increment primary key,
+    id                      bigserial primary key,
     title                   text default 'note',
     description             text default '',
-    group_id                int not null,
+    group_id                bigint not null,
     foreign key (group_id)  references groups(id)
 );
